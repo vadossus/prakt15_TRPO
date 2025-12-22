54d785d4c0d8ab3e85f192175bc91d9c2852aa58
vadossus <vadim.gorowoy2015@gmail.com>
2025-12-22 09:50:43 +0500

    Update 1.0

---
+++ ValidRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace prakt15_TRPO
+{
+    public class ValidRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var input = (value ?? "").ToString().Trim();
+            if (string.IsNullOrEmpty(input))
+            {
+                return new ValidationResult(false, "Поле обязательно для заполнения");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
--- obj/Debug/net8.0-windows/App.g.cs
+++ obj/Debug/net8.0-windows/App.g.cs
@@ -49,7 +49,7 @@ namespace prakt15_TRPO {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -75,7 +75,7 @@ namespace prakt15_TRPO {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public static void Main() {
             prakt15_TRPO.App app = new prakt15_TRPO.App();
             app.InitializeComponent();
